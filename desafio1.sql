e0d188b6cd2c6db63b9d99b2a75e3e5ef1d48801
Mariana <mlobosilva@outlook.com>
2023-01-27 10:53:07 -0300

    Altera nomes das colunas

--- desafio1.sql
+++ desafio1.sql
@@ -4,7 +4,7 @@ DROP DATABASE IF EXISTS SpotifyClone;
 
   CREATE TABLE SpotifyClone.usuarios(
       user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-      usuario tipo restricoes,
+      nome_usuario tipo restricoes,
       idade tipo restricoes,
       plano tipo restricoes,
       data_da_assinatura tipo restricoes,
@@ -13,13 +13,13 @@ DROP DATABASE IF EXISTS SpotifyClone;
 
   CREATE TABLE SpotifyClone.planos(
       plano_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-      plano tipo restricoes,
+      nome_plano tipo restricoes,
       valor_plano tipo restricoes,
   ) engine = InnoDB;
 
   CREATE TABLE SpotifyClone.historico_de_reproducao(
-      usuario tipo restricoes,
-      cancao tipo restricoes,
+      usuario INT,
+      cancao INT,
       data_reproducao tipo restricoes,
       CONSTRAINT PRIMARY KEY(usuario, cancao)
   ) engine = InnoDB;
@@ -32,19 +32,19 @@ DROP DATABASE IF EXISTS SpotifyClone;
 
   CREATE TABLE SpotifyClone.artistas(
       artista_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-      artista tipo restricoes,
+      nome_artista tipo restricoes,
   ) engine = InnoDB;
 
   CREATE TABLE SpotifyClone.albuns(
       album_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-      album tipo restricoes,
+      nome_album tipo restricoes,
       artista tipo restricoes,
       ano_lancamento tipo restricoes,
   ) engine = InnoDB;
 
   CREATE TABLE SpotifyClone.cancoes(
       cancao_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-      nome tipo restricoes,
+      nome_cancao tipo restricoes,
       album tipo restricoes,
       duracao_segundos tipo restricoes,
   ) engine = InnoDB;
